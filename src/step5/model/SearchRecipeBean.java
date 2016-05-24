c07593d92d2534dd0de3d3b77cd56b800601744f
Anis Da Silva Campos <yocorporation@hotmail.com>
2016-05-24 11:36:50 +0200

    Steap 5 : Searh recipebean

--- src/step5/model/SearchRecipeBean.java
+++ src/step5/model/SearchRecipeBean.java
@@ -1,7 +1,22 @@
 package step5.model;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
 /**
  * Created by Anis on 24/05/2016.
  */
-public class SearchRecipeBean {
+@ManagedBean
+@RequestScoped
+public class SearchRecipeBean extends RecipeModel{
+    public static final String ALL_VALUES_STRING="[ALL]";
+    public static final int ALL_VALUES_INT=-2;
+    public SearchRecipeBean() {
+        this.setDescription(ALL_VALUES_STRING);
+        this.setTitle(ALL_VALUES_STRING);
+        this.setType(ALL_VALUES_STRING);
+        this.setExpertise(ALL_VALUES_INT);
+        this.setNbpeople(ALL_VALUES_INT);
+        this.setDuration(ALL_VALUES_INT);
+    }
 }
--- src/step5/model/UserSubmissionModelBean.java
+++ src/step5/model/UserSubmissionModelBean.java
@@ -1,13 +1,14 @@
 package step5.model;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 
 /**
  * Created by Anis on 24/05/2016.
  */
 @ManagedBean
-@SessionScoped
+@RequestScoped
 public class UserSubmissionModelBean extends UserModelBean {
 	private static final long serialVersionUID = 1L;
 
