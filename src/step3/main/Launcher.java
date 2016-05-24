8c84cc7cbfe7d779da561b95df290aba4ff5d752
Antoine Bouquet <antoine.bouquet@mastertp>
2016-05-24 09:36:43 +0200

    modifi launcher

--- src/step3/main/Launcher.java
+++ src/step3/main/Launcher.java
@@ -1,7 +1,15 @@
 package step3.main;
 
+import step3.dao.fabric.DaoFabric;
+import step3.model.UserModelBean;
+
 /**
  * Created by djbranbran on 24/05/16.
  */
 public class Launcher {
+	public static void main(String[] args){
+		UserDAO userDao=DaoFabric.getInstance().createUserDao();
+		RecipesDao recipesDao=DaoFabric.getInstance().createRecipesDao();
+		UserModelBean user1=new UserModelBean("Doe", "John", 55, "jdoe", "pwd");
+	}
 }
