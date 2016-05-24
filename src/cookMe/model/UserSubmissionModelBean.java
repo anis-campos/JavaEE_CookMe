0decd108e9d41c74b19f50e61137907116996f21
djbranbran69 <brandon.oziol@gmail.com>
2016-05-24 15:00:30 +0200

    cookMe

--- src/cookMe/model/UserSubmissionModelBean.java
+++ src/cookMe/model/UserSubmissionModelBean.java
@@ -9,9 +9,23 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean
 @RequestScoped
 public class UserSubmissionModelBean extends UserModelBean {
-	private static final long serialVersionUID = 1L;
+
+	private String pwd1;
 
 	public UserSubmissionModelBean(){
 		
 	}
+
+    public UserSubmissionModelBean(String firstname, String lastname, int age, String login, String password, String email, String pwd1) {
+        super(firstname, lastname, age, login, password, email);
+        this.pwd1 = pwd1;
+    }
+
+    public String getPwd1() {
+        return pwd1;
+    }
+
+    public void setPwd1(String pwd1) {
+        this.pwd1 = pwd1;
+    }
 }
--- src/cookMe/processing/UserControlerBean.java
+++ src/cookMe/processing/UserControlerBean.java
@@ -11,6 +11,8 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import java.util.Map;
+import java.util.regex.Pattern;
+
 
 /**
  * Created by Anis on 24/05/2016.
@@ -43,7 +45,24 @@ public class UserControlerBean {
 	
 	public void checkAndAddUser(UserSubmissionModelBean userSubmitted){
 		//Vérifier les propriétés de l'utilisateur
+		if(userSubmitted != null){
+			if(userSubmitted.getFirstname() != null
+				&& userSubmitted.getLastname() != null
+				//&& userSubmitted.getAge() != null
+				&& userSubmitted.getEmail() != null
+				&& userSubmitted.getLogin() != null
+				&& userSubmitted.getPwd() != null){
 
+                if(Pattern.compile("[a-zA-Z0-9]").matcher(userSubmitted.getFirstname()).matches()
+                        && Pattern.compile("[a-zA-Z0-9]").matcher(userSubmitted.getLastname()).matches()
+                        && userSubmitted.getAge() < 100
+                        && Pattern.compile("[a-zA-Z0-9-._]+@[a-zA-Z0-9-._].[a-z]+").matcher(userSubmitted.getEmail()).matches()
+                        && Pattern.compile("[a-zA-Z0-9-._]").matcher(userSubmitted.getLogin()).matches()
+                        && Pattern.compile("[a-zA-Z0-9]").matcher(userSubmitted.getFirstname()).matches()){
+
+                }
+			}
+		}
 		this.userDao.addUser(userSubmitted);
 	}
 }
