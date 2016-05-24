2cfac49e71a704ffc10f15e8531c94e8d6ee7554
Anis Da Silva Campos <yocorporation@hotmail.com>
2016-05-24 09:44:49 +0200

    Steap 4 : creation des fichiers

---
+++ src/step4/model/LoginBean.java
@@ -0,0 +1,7 @@
+package step4.model;
+
+/**
+ * Created by Anis on 24/05/2016.
+ */
+public class LoginBean {
+}
---
+++ src/step4/processing/RecipeControlerBean.java
@@ -0,0 +1,7 @@
+package step4.processing;
+
+/**
+ * Created by Anis on 24/05/2016.
+ */
+public class RecipeControlerBean {
+}
