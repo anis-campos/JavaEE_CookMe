453109d34a6dfa9d1b01aba1b439eb757920bc61
djbranbran69 <brandon.oziol@gmail.com>
2016-05-25 10:54:04 +0200

    search

--- src/main/java/cookMe/processing/SearchControlerBean.java
+++ src/main/java/cookMe/processing/SearchControlerBean.java
@@ -7,8 +7,11 @@ import cookMe.model.RecipeSubmissionModelBean;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by djbranbran on 25/05/16.
@@ -18,12 +21,14 @@ import java.util.List;
 public class SearchControlerBean {
 
 
-    private RecipeSubmissionModelBean recipeSearch;
     private List<String> listType;
 
     private RecipesDao recipeDao;
 
     public SearchControlerBean() {
+    }
+
+    public String searchRecipe(){
         this.recipeDao = DaoFabric.getInstance().createRecipesDao();
         List<RecipeModelBean>list = this.recipeDao.getAllRecipes();
         for(RecipeModelBean rmb : list){
@@ -31,7 +36,12 @@ public class SearchControlerBean {
                 this.listType.add(rmb.getType());
             }
         }
-
+        //récupère l'espace de mémoire de JSF
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        Map<String, Object> requestMap = externalContext.getRequestMap();
+        //place la liste de recette dans l'espace de mémoire de JSF
+        requestMap.put("listType", listType);
+        return "search.jsf";
     }
 
     public List<String> getListType() {
@@ -42,11 +52,4 @@ public class SearchControlerBean {
         this.listType = listType;
     }
 
-    public RecipeSubmissionModelBean getRecipeSearch() {
-        return recipeSearch;
-    }
-
-    public void setRecipeSearch(RecipeSubmissionModelBean recipeSearch) {
-        this.recipeSearch = recipeSearch;
-    }
 }
