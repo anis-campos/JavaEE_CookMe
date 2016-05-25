a34f0689c1a4b7124165c425e03707fbec9dc758
Anis Da Silva Campos <yocorporation@hotmail.com>
2016-05-25 15:21:25 +0200

    no message

---
+++ src/main/java/cookMe/processing/validators/loginAdminValidator.java
@@ -0,0 +1,34 @@
+package cookMe.processing.validators;
+
+import cookMe.dao.fabric.DaoFabric;
+import cookMe.dao.instance.UserDao;
+import cookMe.model.UserModelBean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import java.util.List;
+
+/**
+ * Created by Anis on 25/05/2016.
+ */
+@FacesValidator("loginAdminMask")
+public class loginAdminValidator implements Validator {
+    @Override
+    public void validate(FacesContext facesContext, UIComponent uiComponent, Object o) throws ValidatorException {
+
+        String submittedValue = facesContext.getExternalContext().getRequestParameterMap().get(uiComponent.getClientId(facesContext));
+
+        UserDao userDao = DaoFabric.getInstance().createUserDao();
+
+        UserModelBean user = userDao.findByLogin(submittedValue);
+
+        if(user.getType()!= UserModelBean.UserType.admin){
+            throw new ValidatorException(new FacesMessage("Pas admin"));
+        }
+
+    }
+}
